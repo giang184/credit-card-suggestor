57cfbcad2cbc1ce1acc8eff12bef7f62abdd8920
Andrew Giang <giang184@gmail.com>
2021-09-17 11:14:48 -0700

    add cardcontrol component fragment to app.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import Header from "./Header";
+import Navbar from './Navbar';
+import CardControl from './CardControl';
 
 function App(){
   return (
     <React.Fragment>
+      <Navbar />
       <Header />
+      <CardControl />
     </React.Fragment>
   );
 }
