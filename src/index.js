bef277765fca53a0193df753151dd8ee5141e7df
Andrew Giang <giang184@gmail.com>
2021-09-17 17:15:23 -0700

    add css

--- src/index.js
+++ src/index.js
@@ -3,7 +3,6 @@ import ReactDOM from 'react-dom';
 import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './components/App';
-import reportWebVitals from './reportWebVitals';
 
 ReactDOM.render(
   <React.StrictMode>
